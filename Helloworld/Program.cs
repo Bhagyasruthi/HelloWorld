b0db463598d593bfb699d3dbee73833346f642cb
rgarikipati <Bhagytirumalasetty@gmail.com>
2020-11-01 17:52:37 +0000

    variables with different datatypes

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -8,6 +8,33 @@ namespace Helloworld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("My second commit!");
+            byte number = 2;
+            int count = 10;
+            float totalprice = 20.12f;
+            char character = 'A';
+            bool  working = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalprice);
+            Console.WriteLine(character);
+            Console.WriteLine(working);
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
+
+
+
+
     }
 }
